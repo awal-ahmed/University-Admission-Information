a4215d5c8dfb5c357cc41d8b22880046b07535dc
awal-ahmed <50808571+awal-ahmed@users.noreply.github.com>
2019-12-18 21:01:36 +0600

    blog without design

--- demoUniversity/demoUniversity/Blog.aspx.cs
+++ demoUniversity/demoUniversity/Blog.aspx.cs
@@ -12,7 +12,7 @@ namespace demoUniversity
     public partial class Blog : System.Web.UI.Page
     {
         SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\Projectdatabase.mdf;Integrated Security=True");
-       
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (connect.State == ConnectionState.Open)
@@ -63,6 +63,7 @@ namespace demoUniversity
                 bt1.Text = "admin";
                 Button1.Visible = true;
                 TextBox1.Visible = true;
+                Session.Add("name", bt1.Text);
                 Response.Write("Admin");
             }
             cons_table();
@@ -83,6 +84,7 @@ namespace demoUniversity
             Button1.Visible = false;
             TextBox1.Visible = false;
             bt1.Text = "Login";
+            Response.Redirect("Blog.aspx");
         }
 
         protected void Button1_Click(object sender, EventArgs e)
@@ -94,7 +96,7 @@ namespace demoUniversity
             cmd.ExecuteNonQuery();
             TextBox1.Controls.Clear();
             Response.Redirect("Blog.aspx");
-            
+
         }
         void cons_table()
         {
@@ -112,21 +114,55 @@ namespace demoUniversity
             {
                 DataTable tables = dataSet.Tables[0];
 
-                int pstno = Convert.ToInt32( tables.Rows[i][0].ToString());
+                int pstno = Convert.ToInt32(tables.Rows[i][0].ToString());
                 TableRow row = new TableRow();
                 /*TableCell cell = new TableCell();
                 cell.Width=5;
                 cell.BackColor = System.Drawing.Color.LightGreen;
                 row.Cells.Add(cell);*/
                 TableCell cell1 = new TableCell();
-                
+
                 cell1.Text = tables.Rows[i][1].ToString();
                 cell1.ForeColor = System.Drawing.Color.Blue;
                 cell1.Font.Size = 17;
-                cell1.Font.Bold = true;
+                cell1.Font.Bold = false;
                 row.Cells.Add(cell1);
 
                 Table2.Rows.Add(row);
+                Button bt = new Button();
+                bt.ID = "bt_" + pstno.ToString() + "_Del";
+                bt.CommandArgument = pstno.ToString();
+                bt.Text = "Delete Blog";
+                bt.BackColor = System.Drawing.Color.Red;
+                bt.ForeColor = System.Drawing.Color.White;
+                bt.Click += new EventHandler(btdel_Click);
+                TableCell cellp = new TableCell();
+                row.Cells.Add(cellp);
+                row.Cells[1].Controls.Add(bt);
+                if (Session["reg"] == null)
+                {
+                    bt.Visible = false;
+                }
+                else if (Session["reg"].ToString() == null)
+                {
+                    bt.Visible = false;
+                }
+                else if (Session["reg"].ToString() == "admin")
+                {
+                    bt.Visible = true;
+                }
+                else if (Session["name"].ToString() == tables.Rows[i][1].ToString())
+                {
+                    bt.Visible = true;
+                }
+                else bt.Visible = false;
+                Table2.Rows.Add(row);
+
+                TableRow row2 = new TableRow();
+                /*TableCell cell = new TableCell();
+                cell.Width=5;
+                cell.BackColor = System.Drawing.Color.LightGreen;
+                row.Cells.Add(cell);*/
 
                 TableRow row1 = new TableRow();
                 /*TableCell cell2 = new TableCell();
@@ -134,7 +170,7 @@ namespace demoUniversity
                 cell2.BackColor = System.Drawing.Color.LightGreen;
                 row1.Cells.Add(cell2);*/
                 TableCell cell11 = new TableCell();
-                
+
 
                 string st = tables.Rows[i][2].ToString();
                 Label lb2 = new Label();
@@ -142,19 +178,19 @@ namespace demoUniversity
                 lb2.Text = st;
                 lb2.Font.Size = 15;
 
-               
+
+
 
                 cell11.Controls.Add(lb2);
 
                 // cell11.Text = st;
-                
+
                 row1.Cells.Add(cell11);
                 Table2.Rows.Add(row1);
-                TableRow row2 = new TableRow();
-                /*TableCell cell = new TableCell();
-                cell.Width=5;
-                cell.BackColor = System.Drawing.Color.LightGreen;
-                row.Cells.Add(cell);*/
+
+
+
+                // Loop through rows
                 TableCell cell12 = new TableCell();
                 cell12.BackColor = System.Drawing.Color.FromArgb(13434879);
                 cell12.Text = "&nbsp;";
@@ -162,8 +198,200 @@ namespace demoUniversity
 
                 Table2.Rows.Add(row2);
 
+
+                //Write comment
+                Response.Clear();
+                String sql1 = "SELECT * FROM cmnt where pstno = '" + pstno + "'";
+                SqlCommand command1 = new SqlCommand(sql1, connect);
+                SqlDataAdapter dataAdapter1 = new SqlDataAdapter(command1);
+                DataSet dataSet1 = new DataSet();
+                dataAdapter1.Fill(dataSet1);
+                int j1 = dataSet1.Tables[0].Rows.Count;
+                for (int i1 = 0; i1 < j1; i1++)
+                {
+                    DataTable tables1 = dataSet1.Tables[0];
+
+                    int cmtno = Convert.ToInt32(tables1.Rows[i1][0].ToString());
+                    TableRow rows = new TableRow();
+                    /*TableCell cell = new TableCell();
+                    cell.Width=5;
+                    cell.BackColor = System.Drawing.Color.LightGreen;
+                    row.Cells.Add(cell);*/
+                    TableCell cell1s = new TableCell();
+
+                    cell1s.Text = tables1.Rows[i1][2].ToString();
+                    cell1s.ForeColor = System.Drawing.Color.Blue;
+                    cell1s.Font.Size = 15;
+                    cell1s.Font.Bold = false;
+                    rows.Cells.Add(cell1s);
+
+
+                    Button bts = new Button();
+                    bts.ID = "btc_" + cmtno.ToString() + "_Del";
+                    bts.CommandArgument = cmtno.ToString();
+                    bts.Text = "Delete Comment";
+                    bts.BackColor = System.Drawing.Color.Red;
+                    bts.ForeColor = System.Drawing.Color.White;
+                    bts.Click += new EventHandler(ctdel_Click);
+                    TableCell cellps = new TableCell();
+                    rows.Cells.Add(cellps);
+                    rows.Cells[1].Controls.Add(bts);
+                    if (Session["reg"] == null)
+                    {
+                        bts.Visible = false;
+                    }
+                    else if (Session["reg"].ToString() == null)
+                    {
+                        bts.Visible = false;
+                    }
+                    else if (Session["reg"].ToString() == "admin")
+                    {
+                        bts.Visible = true;
+                    }
+                    else if (Session["name"].ToString() == tables1.Rows[i1][2].ToString())
+                    {
+                        bts.Visible = true;
+                    }
+                    else bts.Visible = false;
+                    Table2.Rows.Add(rows);
+
+                    TableRow row2s = new TableRow();
+                    /*TableCell cell = new TableCell();
+                    cell.Width=5;
+                    cell.BackColor = System.Drawing.Color.LightGreen;
+                    row.Cells.Add(cell);*/
+
+                    TableRow row1s = new TableRow();
+                    /*TableCell cell2 = new TableCell();
+                    cell2.Width = 5;
+                    cell2.BackColor = System.Drawing.Color.LightGreen;
+                    row1.Cells.Add(cell2);*/
+                    TableCell cell11s = new TableCell();
+
+
+                    string sts = tables1.Rows[i1][3].ToString();
+                    Label lb2s = new Label();
+                    sts = "&nbsp; &nbsp;   " + sts;
+                    lb2s.Text = sts;
+                    lb2s.Font.Size = 13;
+
+
+
+
+                    cell11s.Controls.Add(lb2s);
+
+                    // cell11.Text = st;
+
+                    row1s.Cells.Add(cell11s);
+                    Table2.Rows.Add(row1s);
+
+
+
+                    // Loop through rows
+                    TableCell cell12s = new TableCell();
+                    cell12s.BackColor = System.Drawing.Color.FromArgb(13434879);
+                    cell12s.Text = "&nbsp;";
+                    row2s.Cells.Add(cell12s);
+
+                    Table2.Rows.Add(row2s);
+                }
+                TableRow rows1 = new TableRow();
+                TextBox tb = new TextBox();
+                tb.ID = "tb_" + pstno.ToString() + "_cm";
+
+                Button bt1 = new Button();
+                bt1.CommandArgument = pstno.ToString();
+                bt1.Text = "Comment";
+                bt1.BackColor = System.Drawing.Color.Blue;
+                bt1.ForeColor = System.Drawing.Color.White;
+                bt1.Click += new EventHandler(btcm_Click);
+                TableCell cellpss = new TableCell();
+                rows1.Cells.Add(cellpss);
+                TableCell cellpst = new TableCell();
+                rows1.Cells.Add(cellpst);
+
+                rows1.Cells[1].Controls.Add(bt1);
+                rows1.Cells[0].Controls.Add(tb);
+
+                if (Session["reg"] == null)
+                {
+                    bt1.Visible = false;
+                    tb.Visible = false;
+                }
+                else if (Session["reg"].ToString() == null)
+                {
+                    bt1.Visible = false;
+                    tb.Visible = false;
+                }
+                else if (Session["reg"].ToString() == "admin")
+                {
+                    bt1.Visible = true;
+                    tb.Visible = true;
+                }
+                Table2.Rows.Add(rows1);
+
+
+
+
+
+
+
+
+
             }
-            // Loop through rows
+            Table2.HorizontalAlign = HorizontalAlign.Center;
+
+        }
+        void btdel_Click(object sender, EventArgs e)
+        {
+
+            Button button = (Button)sender;
+            string row = button.CommandArgument;
+            int i = Convert.ToInt32(row);
+            SqlCommand cmd = connect.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from bpost where id = '" + i + "'";
+
+
+
+            cmd.ExecuteNonQuery();
+            Response.Redirect("Blog.aspx");
+
+
+        }
+        void ctdel_Click(object sender, EventArgs e)
+        {
+
+            Button button = (Button)sender;
+            string row = button.CommandArgument;
+            int i = Convert.ToInt32(row);
+            SqlCommand cmd = connect.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from cmnt where id = '" + i + "'";
+
+
+
+            cmd.ExecuteNonQuery();
+            Response.Redirect("Blog.aspx");
+
+
+        }
+        void btcm_Click(object sender, EventArgs e)
+        {
+
+            Button button = (Button)sender;
+            string pstno = button.CommandArgument;
+            int i = Convert.ToInt32(pstno);
+            TextBox txt = (TextBox)Table2.FindControl("tb_" + pstno.ToString() + "_cm");
+            Response.Write(txt.Text.ToString());
+            SqlCommand cmd = connect.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            string st = Session["name"].ToString();
+            cmd.CommandText = "insert into cmnt (name , pstno , cmt) values ('" + st + "','" + i + "','" + txt.Text + "')";
+            cmd.ExecuteNonQuery();
+            TextBox1.Controls.Clear();
+            Response.Redirect("Blog.aspx");
+
 
         }
     }
